ff4c3e899677764036aee7bbecbc0e8f61d0b32c
beckhsu <beckhsu@synology.com>
2016-09-19 09:52:43 +0800

    add group template

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 INSTALL_PATH=/tmp/pdsh/
 GROUP_PATH=$HOME/.dsh/group/
 
-cp pdshrc $HOME/pdshrc
+ln -s pdshrc $HOME/pdshrc
+ln -s dsh $HOME/.dsh
 
 [ ! -d $INSTALL_PATH ] && mkdir -p $INSTALL_PATH && tar -xvf pdsh-2.26.tar.bz2 -C $INSTALL_PATH
 cd ${INSTALL_PATH}pdsh-2.26/
